96b24efec03232d78a7d621683812b3b2bf3fe2a
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-09 13:44:12 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int c;
+int a;
+for (c = 48; c < 58; c++)
+putchar(c);
+for (a = 97; a < 103; a++)
+putchar(a);
+return (0);
+}
