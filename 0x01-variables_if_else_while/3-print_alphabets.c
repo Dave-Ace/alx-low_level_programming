38b7c8570d8260e9cee9587c225ca96a8b56a78d
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-09 07:02:13 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int c;
+int d = 65;
+for (c = 97; c < 123; c++)
+putchar(c);
+putchar(d);
+d++
+putchar(10);
+return (0);
+}
