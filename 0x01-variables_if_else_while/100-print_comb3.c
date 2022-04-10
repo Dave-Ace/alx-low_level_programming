f71fc385514e1c0570d877c3c8eb6a265a7f99ac
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-10 16:20:23 -0700

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: laways 0 (Success)
+ */
+int main(void)
+{
+int f;
+int s;
+int c = 0;
+for (f = 49; f < 58; f++)
+for (s = 49 + c; s < 58; s++)
+if (s == 57)
+++c
+else
+putchar(f);
+putchar(s);
+putchar(44);
+putchar(' ');
+return (0);
+}
