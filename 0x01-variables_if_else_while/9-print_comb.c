0d48598a74d46cff2cb9237c55ac6a9362febf0c
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-10 15:40:28 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stido.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar('0' - 4);
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
