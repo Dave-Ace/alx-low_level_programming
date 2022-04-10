da0becd92ca48f0b03b2184123ef74d63ed533ed
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-10 16:30:27 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,19 @@ int main(void)
 int f;
 int s;
 int c = 0;
-for (f = 49; f < 58; f++)
+for (f = 48; f < 58; f++)
 {
 for (s = 49 + c; s < 58; s++)
 {
 if (s == 57)
+{
 ++c;
+putchar(f, s, 44);
+putchar(' ');
+}
 else
 {
-putchar(f);
-putchar(s);
-putchar(44);
+putchar(f, s, 44);
 putchar(' ');
 }
 }
