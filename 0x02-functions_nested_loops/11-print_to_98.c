70b245df6d78737a0f31939f57fb2aafc49612f4
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-13 09:52:45 -0700

    Battery street, the og

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print to 98
+ * @n: parameter
+ * Return: Void
+ */
+void print_to_98(int n);
+{
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%i, ", n);
+n++;
+}
+}
+else
+{
+while (n >= 98)
+{
+printf("%i, ", n);
+n--;
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+void print_to_98(int n);
 void times_table(void);
 void jack_bauer(void);
 int add (int a, int b);
