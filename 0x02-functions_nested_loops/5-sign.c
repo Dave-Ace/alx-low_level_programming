c28ae951f14e875c04a33059b48a5914a16bed67
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-12 17:47:59 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,21 @@
  */
 int print_sign(int n)
 {
+int r;
 if (n > 0)
 {
-putchar('+');
-return (1);
+_putchar('+');
+r = 1;
 }
 else if (n == 0)
 {
-putchar('0');
-return (0);
+_putchar('0');
+r = 0;
 }
 else
 {
-putchar('-');
-return (-1);
+_putchar('-');
+r = -1;
 }
+return (r);
 }
