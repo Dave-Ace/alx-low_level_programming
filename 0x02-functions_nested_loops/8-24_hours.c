bf22ffe5d4ec7dbb8ffed400dc8a92d9a6de63d2
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-13 04:30:52 -0700

    timem correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,32 +5,21 @@
  */
 void jack_bauer(void)
 {
-int m1 = 0;
-int m2 = 0;
-int h = 0;
-while (h < 24)
-{
-if (h < 10)
-{
-_putchar('0');
-_putchar(h + '0');
-_putchar(':');
-}
-else
+int m1;
+int m2;
+int h;
+while (h < 25)
 {
 _putchar(h + '0');
 _putchar(':');
-}
-while (m2 < 5)
+for (m2 = 48; m2 < 54; m2++)
 {
 _putchar(m2 + '0');
-while (m1 < 10)
+for (m1 = 48; m1 < 58; m1++)
 {
-_putchar(m1 + '0');
+_putcharr(m1 + '0');
 _putchar('\n');
-m1++;
 }
-m2++;
 }
 h++;
 }
