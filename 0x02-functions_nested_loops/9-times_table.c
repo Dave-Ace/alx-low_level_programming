843bd70522c51920554c81c1292b6d958625036b
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-13 09:21:35 -0700

    times table done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@ void times_table(void)
 {
 int t = 0;
 int tt;
+int nxt;
 while (t < 10)
 {
 tt = 0;
@@ -26,7 +27,8 @@ if (tt != 9)
 {
 _putchar(',');
 _putchar(' ');
-if ((times <= 9) && ((tt++ * t) <= 9))
+nxt = tt++;
+if ((times <= 9) && ((nxt *t) <= 9))
 {
 _putchar(' ');
 }
