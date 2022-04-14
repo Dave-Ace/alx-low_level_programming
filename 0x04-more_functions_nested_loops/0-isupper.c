9269daa86729ab6e09795e7e9543da8108e29590
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-14 12:00:24 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_issupper -- To check for upper case
+ * @c: pArameter to check
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if ((_putchar(c) > 64) || (_putchar(c) < 91))
+r = 1
+else
+r = 0
+return (r);
+}
