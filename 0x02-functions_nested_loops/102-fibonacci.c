9d26d1ac7776ffd7f5dd53c382f296f6f9976f29
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-17 15:58:04 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,21 +3,16 @@
  * main - fibonacci
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int n = 1;
-int f, s, nxt;
+int f = 1, s = 1, nxt;
 while (n <= 50)
 {
 if (n == 1)
 {
 f = 1;
 printf("%i, ", f);
-}
-if (n == 2)
-{
-s = 1;
-printf("%i, ", s);
 }
 nxt = f + s;
 printf("%i, ", nxt);
