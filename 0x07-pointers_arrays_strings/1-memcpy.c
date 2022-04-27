3ef2d87913a68380bad44c0d98e2e26c3a716ad5
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-27 05:07:37 -0700

    memcpy library

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memcpy - copies memory area to another memory area
+ * @dest:destination 
+ * @src: Sources
+ * @n: number of bytes
+ * Return: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+memcpy(dest, src, n);
+return(dest);
+}
