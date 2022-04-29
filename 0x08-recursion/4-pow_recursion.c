44f6e439ebe453394dffadb4109b4273f89d4a9e
Toluwani <davidoyekanmi.t@gmail.com>
2022-04-29 02:52:07 -0700

    recursive power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - raise a value to the power of another value
+ * @x: value to be raised
+ * @y: ower
+ * Return: value raised
+ */
+int _pow_recursion(int x, int y)
+{
+if (y = 0)
+return (1);
+if (y < 0)
+return (-1);
+return (x * _pow_recursion(x, y - 1);
+}
