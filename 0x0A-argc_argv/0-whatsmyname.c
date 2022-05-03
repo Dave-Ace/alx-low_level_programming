a3d5868680fe4ed5abe5990b13e298818420be85
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-03 14:30:45 -0700

    argc| git push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument
+ * @argv: argument array
+ * Return: 0
+ */
+int main (int argc, char **argv )
+{
+printf("%s\n", *argv[0]);
+return (0);
+}
