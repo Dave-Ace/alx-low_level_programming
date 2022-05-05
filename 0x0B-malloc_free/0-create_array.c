852d0e32b7973d3f95460d5c298362963525ac52
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-05 16:52:04 -0700

    mallow

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,7 +8,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-int i;
+unsigned int i;
 char *ar;
 if (size == 0)
 {
@@ -22,5 +22,5 @@ for (i = 0; i <= size; i++)
 ar[i] = c;
 }
 }
-return (*ar);
+return (ar);
 }
