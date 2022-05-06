8f3b457431e5c9326d384fd29010531ea66f6670
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-05 17:28:35 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function that creates an array
+ * @str: string
+ * Return: Char
+ */
+char *_strdup(char *str)
+{
+int c[] = str;
+int i;
+char *ar;
+ar = malloc(sizeof(*str) * strlen(str));
+if (ar == NULL)
+return (NULL);
+for (i = 0; i < strlen(str); i++)
+{
+ar[i] = c[i];
+}
+return (ar);
+}
