a135276748b673503412269d6ad58e53ed571ebd
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-07 13:45:41 -0700

    coincs

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,12 +9,25 @@
  */
 int main(int argc, char **argv)
 {
-int cent = atoi(argv[1]);
+int cents = atoi(argv[1]);
+int coins = 0;
 if (argc == 1 || argc > 2)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", cent / 10);
+while (cents > 0)
+{
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+coins += 1;
+}
+printf("%d\n", coins);
 return (0);
 }
