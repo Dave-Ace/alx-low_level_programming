0bf52a361d3f0fa463a8b768c35e92b9f7005731
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-07 12:26:16 -0700

    argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,8 +6,10 @@
  * @argv: argument array
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc, char **argv)
 {
-printf("%s\n", argv[0]);
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
