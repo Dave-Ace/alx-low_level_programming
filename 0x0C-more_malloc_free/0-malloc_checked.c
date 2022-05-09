75621dd1cd12bc77e3368c0546cc830329e6cf43
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-09 15:11:14 -0700

    malloc and free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - allocates memmory
+ * *malloc_checked - allocates memmory
  * @b: variable
  */
 void *malloc_checked(unsigned int b)
 {
 int s = malloc(b);
+if (s == 0)
+return (0);   
 }
