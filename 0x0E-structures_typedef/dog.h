b07718f33c4a962d7e9a582d6e1f9c35a3d8df1f
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-11 17:39:39 -0700

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owner
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+/**
+ * dog_t - type dog
+ */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
