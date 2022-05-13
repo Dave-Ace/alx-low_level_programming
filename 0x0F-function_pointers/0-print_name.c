b4d9546d1c40e037c0b55e13091e18157955c21a
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-13 16:38:10 -0700

    pointer to functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 /**
  * print_name - function to pointer that prints name
  * @name: name to print
@@ -8,7 +7,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 f = print_name_as_is;
-f(name);
+f(*name);
 f = print_name_uppercase;
-f(name);
+f(*name);
 }
