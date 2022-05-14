0cee0d5e1758d032c0359048434af6b8cfb9734c
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-13 17:17:18 -0700

    function that searches for aninteger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - function that searches for an integer
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int i;
+if (size <= 0)
+{
+return (-1);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
+}
