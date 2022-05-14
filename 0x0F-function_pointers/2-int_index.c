a58c635f937cfb867d2ff02f617d7e6d8e4b2e67
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-13 17:22:23 -0700

    func_point

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,17 +8,17 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <= 0)
-{
-return (-1);
-}
-else
+if (array && cmp)
 {
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
 return (i);
 }
+return (-1);
+}
+else
+{
 return (-1);
 }
 }
