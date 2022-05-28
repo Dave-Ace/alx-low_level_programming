86d9553e0d41c20a2a94889a8850fa25897b78ce
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-28 10:32:00 -0700

    add nodes to end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,7 +24,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (preNode != NULL)
 	{
 		while (preNode->next != NULL)
-			PreNode = preNode->next;
+			preNode = preNode->next;
 	}
 	preNode->next = newNode;
 	newNode->len = count;
