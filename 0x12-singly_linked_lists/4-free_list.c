c081948c0e928e7f035383d59aaa57c8e565b41e
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-28 14:58:18 -0700

    free nodes

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - function that free list
+ * @head: point4er to list 
+ */
+void free_list(list_t *head)
+{
+	list_t *nodes;
+
+	while (nodes != NULL)
+	{
+		node = head->next;
+		free(node);
+		head = node;
+	}
+}
