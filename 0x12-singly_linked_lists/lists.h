303f4b162d7a618e9bb5579f035a0ca9a6c74fcf
Toluwani <davidoyekanmi.t@gmail.com>
2022-05-28 08:17:27 -0700

    singly linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,4 +2,17 @@
 #define LIST_H
 size_t print_lists(const list_t *h);
 int _putchar(char c);
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list ndoe structure
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 #endif
